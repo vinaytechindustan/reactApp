0f36c6cf444a0ad4bc297dface1e490ee672156d
Vinay <vinay.techindustan@gmail.com>
2018-07-10 11:51:11 +0530

    just checking index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MApp from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MApp />, document.getElementById('root'));
 registerServiceWorker();

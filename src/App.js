b66babe9a56c4cb93accee0d15aed93d8ddb6a2d
Vinay <vinay.techindustan@gmail.com>
2018-07-10 18:43:24 +0530

    first if

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import Person from './Components/Person';
 
 class App extends Component {
   state={
-    persons:[{name:"Shashwat",age:5},{name:"Sandeep Kumar",age:12},{name:"sahil",age:15}]
+    persons:[{name:"Shashwat",age:5},{name:"Sandeep Kumar",age:12},{name:"sahil",age:15}],
+    showme:true,
   }
   nameHandler=(nam)=>{
     this.setState({ persons:[{name:nam,age:5},{name:"Sandeep Kumar",age:12},{name:"sahil",age:995}]});
     console.log("button was clicked");
+  }
+  toggleHandler=()=>{
+    const togs=this.state.showme;
+    this.setState({showme:!togs});
+    
   }
   render() {
     const title ='dk';
@@ -16,11 +22,17 @@ class App extends Component {
       <div>
       <h1> First React </h1>
       <h1> hey {title}</h1>
-      <button onClick={this.nameHandler.bind(this,'Harsh')}>Click </button>
-      <Person className="App" name={this.state.persons[0].name} age={this.state.persons[0].age}> I am feeling good </Person>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={()=>this.nameHandler('Max')}> </Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> </Person>
-      </div>
+      <button onClick={this.toggleHandler}>Click </button>
+      { 
+        this.state.showme ?
+          <div>
+          <Person className="App" name={this.state.persons[0].name} age={this.state.persons[0].age}> I am feeling good </Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={()=>this.nameHandler('Max')}> </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> </Person>
+          </div>: null
+      }
+          </div> 
+      
     );
   }
 }

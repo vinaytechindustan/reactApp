c804e8c39334fde54a2be688a5db34beefc8d482
Vinay <vinay.techindustan@gmail.com>
2018-07-11 13:53:08 +0530

    flexible and dynamic two way binding

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Person from './Components/Person';
 
 class App extends Component {
   state={
-    persons:[{name:"Shashwat",age:5},{name:"Sandeep Kumar",age:12},{name:"sahil",age:15}],
+    persons:[{id:"kd",name:"Shashwat",age:5},{id:"bd",name:"Sandeep Kumar",age:12},{id:"dd",name:"sahil",age:15}],
     showme:true,
   }
-  nameHandler=(nam)=>{
+/*   nameHandler=(nam)=>{
     this.setState({ persons:[{name:nam,age:5},{name:"Sandeep Kumar",age:12},{name:"sahil",age:995}]});
     console.log("button was clicked");
-  }
+  } */
   toggleHandler=()=>{
     const togs=this.state.showme;
     this.setState({showme:!togs});
@@ -22,6 +22,16 @@ class App extends Component {
     persons.splice(index,1);
     this.setState({persons})
 
+  }
+  changeHandler=(event,id)=>{
+   var pIndex=this.state.persons.findIndex(p=> p.id===id);
+   const ersons=[...this.state.persons];
+    var  person=ersons[pIndex];
+    console.log(person);
+    console.log("index value "+pIndex);
+    person.name=event.target.value;
+    ersons[pIndex]=person;
+    this.setState({persons:ersons}); 
   }
   render() {
     const title ='dk';
@@ -35,7 +45,8 @@ class App extends Component {
           <div>
             {this.state.persons.map(
               (person,index)=>{
-                return <Person click={()=>this.deletePersonHandler(index)} name={person.name} age={person.age}></Person>
+                return <Person click={()=>this.deletePersonHandler(index)} name={person.name} 
+                age={person.age} key={person.id} chan={(event)=>this.changeHandler(event,person.id)}></Person>
               }
             )}
 
--- src/Components/Person.js
+++ src/Components/Person.js
@@ -3,9 +3,12 @@ import '../App.css';
 class Person extends Component {
     render(){
         return(
-            <p onDoubleClick={this.props.click}> Hey, I am {this.props.name} and {this.props.age } years old !
-            {this.props.children}
-            </p>
+            <div>
+                <input type="text" onChange={this.props.chan}/>
+                <p onDoubleClick={this.props.click}> Hey, I am {this.props.name} and {this.props.age } years old !
+                {this.props.children}
+                </p>
+            </div>
         )
     }
 }
